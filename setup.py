f241e9d2363c7f075b18d81405cf7427cd15bcb6
Edward Jones <mail@erj4.uk>
2021-03-25 20:54:43 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name="spotifyutils",
     version="0.1",
     py_modules="spotifyutils",
+    packages=find_packages("."),
+    #package_dir={"": "."},
     install_requires=[
         "Click",
         "pyyaml",
         "spotipy"
     ],
-    entry_points='''
-        [console_scripts]
-        spotifyutils=spotifyutils:cli
-    '''
+    entry_points={
+        "console_scripts": ["spotifyutils=spotifyutils:cli"]
+    }
 )
